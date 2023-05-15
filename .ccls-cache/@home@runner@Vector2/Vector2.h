e831bc063d5350b454c75345d5097bdb461a4cab
yunrock <vladimir.1416@gmail.com>
2023-05-15 05:30:10 +0000

    Más funciones que nunca

--- .ccls-cache/@home@runner@Vector2/Vector2.h
+++ .ccls-cache/@home@runner@Vector2/Vector2.h
@@ -1,7 +1,7 @@
 /**
  * <author> Yunuén Vladimir 
- * <size> https://yunuenvladimir.com
- * <source> https://www.github.com/
+ * <site> https://yunuenvladimir.com
+ * <source> https://github.com/yunrock/Vector2CplusplusClass
  * <license> CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0/>
  *
  * Vector2 is a class to handle vectors of two coordinates,
@@ -24,8 +24,8 @@ using std::cout;
 namespace Vector2D {
   class Vector2 {
     private:
-      float m_xValue;
-      float m_yValue;
+      float m_xValue; /**< stores the x Vector2 coordinate value */
+      float m_yValue; /**< stores the y Vector2 coordinate value */
       float m_magnitude;
       float m_direction; 
 
@@ -40,16 +40,26 @@ namespace Vector2D {
           Vector2* m_pParent;
           Coordenate(float& t_value, Vector2* t_m_pParent) : 
                      m_rValue(t_value), m_pParent(t_m_pParent) {}
-          operator const float& () const { 
-            return m_rValue; 
-          }
       
         public:
+          /**
+           * Here is where all the magic hapen. Every time it uses the = operator to update
+           * a coordinate of the vector, the magnitud and the direction of itself
+           * is also updated.
+           */
           Coordenate& operator=(const float& t_val) {
             m_rValue = t_val;
             m_pParent->m_Magnitude();
             m_pParent->m_Direction();
             return *this;
+          }
+          /**
+           * This kind of use of the operator () allow us to
+           * get access to the m_rValue, which means that we can
+           * get the coordinate value in the Vector2 class
+           */
+          operator const float& () const { 
+            return m_rValue; 
           }
         friend class Vector2;
       };
@@ -63,14 +73,31 @@ namespace Vector2D {
         x = t_x;
         y = t_y;
       }
+
       float magnitude();
       float direction(); 
-      void normalize(); 
+      void normalize();
+      void rotate(const float& t_rAngle);
+
       void operator =(const Vector2& t_rVector);
-      Vector2 operator *(const float& t_rScalar);
       Vector2 operator +(const Vector2& t_rVector);
       Vector2 operator -(const Vector2& t_rVector);
-      static float sDotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2);
+      Vector2 operator *(const float& t_rScalar);
+      Vector2 operator /(const float& t_rScalar);
+      void operator +=(const Vector2& t_rVector);
+      void operator -=(const Vector2& t_rVector);
+      void operator *=(const float& t_rScalar);
+      void operator /=(const float& t_rScalar);
+      bool operator ==(const Vector2& t_rVector);//////
+      bool operator !=(const Vector2& t_rVector);//////
+      bool operator <(const Vector2& t_rVector);//////
+      bool operator >(const Vector2& t_rVector);//////
+      bool operator <=(const Vector2& t_rVector);//////
+      bool operator >=(const Vector2& t_rVector);//////
+
+      static float dotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2);
+      static float distance(const Vector2& t_rVector1, const Vector2& t_rVector2);
+      static Vector2 rotate(const Vector2& t_rVector, const float& t_rAngle); //////
   };
 }
 
@@ -78,38 +105,64 @@ namespace Vector2D {
 using namespace Vector2D;
 
 /**
- *
+ * Member function that calculates the magnitud of the vector
  */
 void Vector2::m_Magnitude() {
   m_magnitude = sqrt( (pow(m_xValue,2) + pow(m_yValue, 2)) );
 }
 
 /**
- *
+ * Member function that calculates the direction of the vector
  */
 void Vector2::m_Direction() {
-        
+  if (m_xValue == 0) {
+    if (m_yValue == 0) {
+      m_direction = 0;
+      return;
+    }
+    if (m_yValue > 0) {
+      m_direction = 90;
+      return;
+    }
+    if (m_yValue < 0) {
+      m_direction = 180;
+      return;
+    }
+  }
+  m_direction = atan(m_yValue / m_xValue);
 }
 
 /**
- *
+ * @returns a float value that is the magnitud of the vector
  */
 float Vector2::magnitude() {
   return m_magnitude;
 }
 
 /**
- *
+ * @returns a float value that is the direction of the vector
  */
 float Vector2::direction() {
   return m_direction;
 }
 
 /**
- *
+ * Normalizes the current vector
  */
 void Vector2::normalize() {
-  
+  x = m_xValue / m_magnitude;  
+  y = m_yValue / m_magnitude;  
+}
+
+/**
+ * Rotates the current vector based in the formula
+ * x' = x * cos(t_rAngle) - y * sen(t_rAngle) 
+ * y' = x * sen(t_rAngle) + y * cos(t_rAngle) 
+ * @param t_rAngle is the angle to rotate the vector
+ */
+void Vector2::rotate(const float& t_rAngle) {
+  x = (m_xValue * cos(t_rAngle)) - (m_yValue * sin(t_rAngle));  
+  y = (m_xValue * sin(t_rAngle)) + (m_yValue * cos(t_rAngle));  
 }
 
 /**
@@ -126,6 +179,24 @@ void Vector2::operator =(const Vector2& t_rVector) {
  * @param
  * @return
  */
+Vector2 Vector2::operator +(const Vector2& t_rVector) {
+  return Vector2(this->x + t_rVector.m_xValue, this->y + t_rVector.m_yValue);
+}
+
+/**
+ * 
+ * @param
+ * @return
+ */
+Vector2 Vector2::operator -(const Vector2& t_rVector) {
+  return Vector2(this->x - t_rVector.m_xValue, this->y - t_rVector.m_yValue);
+}
+
+/**
+ * 
+ * @param
+ * @return
+ */
 Vector2 Vector2::operator *(const float& t_rScalar) {
   return Vector2(this->x * t_rScalar, this->y * t_rScalar);
 }
@@ -135,8 +206,46 @@ Vector2 Vector2::operator *(const float& t_rScalar) {
  * @param
  * @return
  */
-Vector2 Vector2::operator +(const Vector2& t_rVector) {
-  return Vector2(this->x + t_rVector.m_xValue, this->y + t_rVector.m_yValue);
+Vector2 Vector2::operator /(const float& t_rScalar) {
+  return Vector2(this->x * t_rScalar, this->y * t_rScalar);
+}
+
+/**
+ * 
+ * @param
+ */
+void Vector2::operator +=(const Vector2& t_rVector) {
+  this->x = this->x + t_rVector.m_xValue;
+  this->y = this->y + t_rVector.m_yValue;
+}
+
+/**
+ * 
+ * @param
+ */
+void Vector2::operator -=(const Vector2& t_rVector) {
+  this->x = this->x - t_rVector.m_xValue;
+  this->y = this->y - t_rVector.m_yValue;
+}
+
+/**
+ * 
+ * @param
+ * @return
+ */
+void Vector2::operator *=(const float& t_rScalar) {
+  this->x = this->x * t_rScalar;
+  this->y = this->y * t_rScalar;
+}
+
+/**
+ * 
+ * @param
+ * @return
+ */
+void Vector2::operator /=(const float& t_rScalar) {
+  this->x = this->x / t_rScalar;
+  this->y = this->y / t_rScalar;
 }
 
 /**
@@ -145,6 +254,21 @@ Vector2 Vector2::operator +(const Vector2& t_rVector) {
  * @param
  * @return
  */
-float Vector2::sDotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2) {
+float Vector2::dotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2) {
   return (t_rVector1.m_xValue * t_rVector2.m_xValue) + (t_rVector1.m_yValue * t_rVector2.m_yValue);
+}
+
+/**
+ * 
+ * @param
+ * @param
+ * @return
+ */
+float Vector2::distance(const Vector2& t_rVector1, const Vector2& t_rVector2) {
+  float xTmp = t_rVector2.m_xValue - t_rVector1.m_xValue;
+  float yTmp = t_rVector2.m_yValue - t_rVector1.m_yValue;
+  if (xTmp == 0 && yTmp == 0) {
+    return 0;
+  }
+  return sqrt ( powf(xTmp, 2) + powf(yTmp, 2) );
 }
--- .ccls-cache/@home@runner@Vector2/main.cpp
+++ .ccls-cache/@home@runner@Vector2/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
   Vector2 v2(1, 1);
-  Vector2 v3(1, 1);
+  Vector2 v3(3, 4);
   v2.x = 3;
-  v2.y = 4;
+  v2.y = 5;
   //v3 = v2 * 10;
   //float f = Vector2::dotProduct(v2, v3);
-  std::cout << v3.magnitude() << "  Hello World!\n";
+  std::cout << Vector2::distance(v2, v3) << "  Hello World!\n"; 
 }
