4ac273955324b071984d801502571bfeeb3657c9
yunrock <vladimir.1416@gmail.com>
2023-05-17 04:10:41 +0000

    Version 1 terminada

--- Vector2.h
+++ Vector2.h
@@ -84,20 +84,22 @@ namespace Vector2D {
       Vector2 operator -(const Vector2& t_rVector);
       Vector2 operator *(const float& t_rScalar);
       Vector2 operator /(const float& t_rScalar);
+
       void operator +=(const Vector2& t_rVector);
       void operator -=(const Vector2& t_rVector);
       void operator *=(const float& t_rScalar);
       void operator /=(const float& t_rScalar);
-      bool operator ==(const Vector2& t_rVector);//////
-      bool operator !=(const Vector2& t_rVector);//////
-      bool operator <(const Vector2& t_rVector);//////
-      bool operator >(const Vector2& t_rVector);//////
-      bool operator <=(const Vector2& t_rVector);//////
-      bool operator >=(const Vector2& t_rVector);//////
+      bool operator ==(const Vector2& t_rVector);
+      bool operator !=(const Vector2& t_rVector);
+      bool operator <(const Vector2& t_rVector);
+      bool operator >(const Vector2& t_rVector);
+      bool operator <=(const Vector2& t_rVector);
+      bool operator >=(const Vector2& t_rVector);
 
       static float dotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2);
       static float distance(const Vector2& t_rVector1, const Vector2& t_rVector2);
-      static Vector2 rotate(const Vector2& t_rVector, const float& t_rAngle); //////
+      static float angleBetween(const Vector2& t_rVector1, const Vector2& t_rVector2); 
+      static Vector2 rotate(const Vector2& t_rVector, const float& t_rAngle); 
   };
 }
 
@@ -166,8 +168,8 @@ void Vector2::rotate(const float& t_rAngle) {
 }
 
 /**
- * 
- * @param
+ * Asigns the values of the parameter vector to the current vector 
+ * @param t_rVector vector from wich values will be copied
  */
 void Vector2::operator =(const Vector2& t_rVector) {
   this->x = t_rVector.m_xValue;
@@ -175,44 +177,44 @@ void Vector2::operator =(const Vector2& t_rVector) {
 }
 
 /**
- * 
- * @param
- * @return
+ * Sum of two vectors
+ * @param t_rVector is a constant reference
+ * @return a new Vector2 object 
  */
 Vector2 Vector2::operator +(const Vector2& t_rVector) {
   return Vector2(this->x + t_rVector.m_xValue, this->y + t_rVector.m_yValue);
 }
 
 /**
- * 
- * @param
- * @return
+ * substraction of two vectors
+ * @param t_rVector is a constant reference
+ * @return a new Vector2 object 
  */
 Vector2 Vector2::operator -(const Vector2& t_rVector) {
   return Vector2(this->x - t_rVector.m_xValue, this->y - t_rVector.m_yValue);
 }
 
 /**
- * 
- * @param
- * @return
+ * Scalar product
+ * @param t_rScalar const float reference
+ * @return a new Vector2 object
  */
 Vector2 Vector2::operator *(const float& t_rScalar) {
   return Vector2(this->x * t_rScalar, this->y * t_rScalar);
 }
 
 /**
- * 
- * @param
- * @return
+ * Division by a number
+ * @param t_rScalar const float reference
+ * @return a new Vector2 object
  */
 Vector2 Vector2::operator /(const float& t_rScalar) {
   return Vector2(this->x * t_rScalar, this->y * t_rScalar);
 }
 
 /**
- * 
- * @param
+ * Replace the coordinates values by the adition of the @param
+ * @param t_rVector is a constant reference
  */
 void Vector2::operator +=(const Vector2& t_rVector) {
   this->x = this->x + t_rVector.m_xValue;
@@ -220,8 +222,8 @@ void Vector2::operator +=(const Vector2& t_rVector) {
 }
 
 /**
- * 
- * @param
+ * Replace the coordinates values by the substraction of the @param
+ * @param t_rVector is a constant reference
  */
 void Vector2::operator -=(const Vector2& t_rVector) {
   this->x = this->x - t_rVector.m_xValue;
@@ -229,9 +231,8 @@ void Vector2::operator -=(const Vector2& t_rVector) {
 }
 
 /**
- * 
- * @param
- * @return
+ * Replace the coordinates values by the scalar product of the @param
+ * @param t_rScalar const float reference
  */
 void Vector2::operator *=(const float& t_rScalar) {
   this->x = this->x * t_rScalar;
@@ -239,9 +240,8 @@ void Vector2::operator *=(const float& t_rScalar) {
 }
 
 /**
- * 
- * @param
- * @return
+ * Replace the coordinates values by the adition of the @param
+ * @param t_rScalar const float reference
  */
 void Vector2::operator /=(const float& t_rScalar) {
   this->x = this->x / t_rScalar;
@@ -249,20 +249,78 @@ void Vector2::operator /=(const float& t_rScalar) {
 }
 
 /**
- * 
- * @param
- * @param
- * @return
+ * Compares the coordinates by the @param coordinates
+ * @param t_rVector to be compared by values
+ * @return return true if x = @param.x AND y = @param.y
+ */
+bool Vector2::operator ==(const Vector2& t_rVector) {
+  return (this->m_xValue == t_rVector.m_xValue) && (this->m_yValue == t_rVector.m_yValue);
+}
+
+/**
+ * Compares the coordinates by the @param coordinates
+ * @param t_rVector to be compared by values
+ * @return return false if x = @param.x AND y = @param.y
+ */
+bool Vector2::operator !=(const Vector2& t_rVector) {
+  return !(*this == t_rVector);
+}
+
+/**
+ * Compares the magnitude whit the @param magnitude
+ * @param t_rVector to be compared by magnitude
+ * @return return true if the magnitud of vector 
+ * is strictly less than the @param magnitude
+ */
+bool Vector2::operator <(const Vector2& t_rVector) {
+  return this->m_magnitude < t_rVector.m_magnitude;
+}
+
+/**
+ * Compares the magnitude whit the @param magnitude
+ * @param t_rVector to be compared by magnitude
+ * @return return true if the magnitud of vector 
+ * is strictly bigger than the @param magnitude
+ */
+bool Vector2::operator >(const Vector2& t_rVector) {
+  return this->m_magnitude > t_rVector.m_magnitude;
+}
+
+/**
+ * Compares the magnitude whit the @param magnitude
+ * @param t_rVector to be compared by magnitude
+ * @return return true if the magnitud of vector 
+ * is less or equals than the @param magnitude
+ */
+bool Vector2::operator <=(const Vector2& t_rVector) {
+  return this->m_magnitude <= t_rVector.m_magnitude;
+}
+
+/**
+ * Compares the magnitude whit the @param magnitude
+ * @param t_rVector to be compared by magnitude
+ * @return return true if the magnitud of vector 
+ * is bigger or equals than the @param magnitude
+ */
+bool Vector2::operator >=(const Vector2& t_rVector) {
+  return this->m_magnitude <= t_rVector.m_magnitude;
+}
+
+/**
+ * Dot product
+ * @param t_rVector1 is a constant reference
+ * @param t_rVector2 is a constant reference
+ * @return float
  */
 float Vector2::dotProduct(const Vector2& t_rVector1, const Vector2& t_rVector2) {
   return (t_rVector1.m_xValue * t_rVector2.m_xValue) + (t_rVector1.m_yValue * t_rVector2.m_yValue);
 }
 
 /**
- * 
- * @param
- * @param
- * @return
+ * Distance between two vectors
+ * @param t_rVector1 is a constant reference
+ * @param t_rVector2 is a constant reference
+ * @return float
  */
 float Vector2::distance(const Vector2& t_rVector1, const Vector2& t_rVector2) {
   float xTmp = t_rVector2.m_xValue - t_rVector1.m_xValue;
@@ -271,4 +329,30 @@ float Vector2::distance(const Vector2& t_rVector1, const Vector2& t_rVector2) {
     return 0;
   }
   return sqrt ( powf(xTmp, 2) + powf(yTmp, 2) );
+}
+
+/**
+ * Calculates the angle between two vectors using @see dotPtoduct
+ * @param t_rVector1 is a constant reference
+ * @param t_rVector2 is a constant reference
+ * @return float resulting of acos(dotPtoduct(t_rVector1, t_rVector2) / product of their magnitudes)
+ */
+float Vector2::angleBetween(const Vector2& t_rVector1, const Vector2& t_rVector2) {
+  float xTmp = t_rVector2.m_xValue - t_rVector1.m_xValue;
+  float yTmp = t_rVector2.m_yValue - t_rVector1.m_yValue;
+  if (xTmp == 0 && yTmp == 0) {
+    return 0;
+  }
+  return acos(dotProduct(t_rVector1, t_rVector2) / (t_rVector1.m_magnitude * t_rVector2.m_magnitude));
+}
+
+/**
+ * Rotates a vector through @param angle
+ * @param t_rVector1 is a constant reference
+ * @param t_rfloat is a constant reference
+ * @return new vector @param angle rotated
+ */
+Vector2 Vector2::rotate(const Vector2& t_rVector1, const float& t_rAngle) {
+  return Vector2((cos(t_rAngle) * t_rVector1.m_xValue) - (sin(t_rAngle) * t_rVector1.m_yValue),
+                 (sin(t_rAngle) * t_rVector1.m_xValue) + (cos(t_rAngle) * t_rVector1.m_yValue));
 }
